0726fd84ead76df402ed2f4a01318afe12534e1c
AgostaSalvatore <agosta.boolean@gmail.com>
2025-08-11 17:18:34 +0200

    feat: add header component with DC logo and navigation menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,34 +7,34 @@
             <nav>
                 <ul class="d-flex justify-content-around list-unstyled gap-3 mb-0">
                     <li>
-                        <a href="">CHARACTERS</a>
+                        <a href="" class="text-decoration-none text-black">CHARACTERS</a>
                     </li>
                     <li>
-                        <a href="">COMICS</a>
+                        <a href="" class="text-decoration-none text-black">COMICS</a>
                     </li>
                     <li>
-                        <a href="">MOVIES</a>
+                        <a href="" class="text-decoration-none text-black">MOVIES</a>
                     </li>
                     <li>
-                        <a href="">TV</a>
+                        <a href="" class="text-decoration-none text-black">TV</a>
                     </li>
                     <li>
-                        <a href="">GAMES</a>
+                        <a href="" class="text-decoration-none text-black">GAMES</a>
                     </li>
                     <li>
-                        <a href="">COLLECTIBLES</a>
+                        <a href="" class="text-decoration-none text-black">COLLECTIBLES</a>
                     </li>
                     <li>
-                        <a href="">VIDEOS</a>
+                        <a href="" class="text-decoration-none text-black">VIDEOS</a>
                     </li>
                     <li>
-                        <a href="">FANS</a>
+                        <a href="" class="text-decoration-none text-black">FANS</a>
                     </li>
                     <li>
-                        <a href="">NEWS</a>
+                        <a href="" class="text-decoration-none text-black">NEWS</a>
                     </li>
                     <li>
-                        <a href="">SHOP</a>
+                        <a href="" class="text-decoration-none text-black">SHOP</a>
                     </li>
                 </ul>
             </nav>
